3333cb37f83917808500da34930c8b8060b31892
Jonas Møller <jonas.moller@student.uib.no>
2021-03-23 13:24:19 +0100

    Bump flycheck-posframe
    Fixes alexmurray/flycheck-posframe#27

--- modules/checkers/syntax/packages.el
+++ modules/checkers/syntax/packages.el
@@ -4,6 +4,6 @@
 (package! flycheck :pin "01396a5eff9fa494285e0d3139838231c05e3948")
 (package! flycheck-popup-tip :pin "ef86aad907f27ca076859d8d9416f4f7727619c6")
 (when (featurep! +childframe)
-  (package! flycheck-posframe :pin "24fd9b3d81eab8dd850c504ae46a5c5f11a46ee0"))
+  (package! flycheck-posframe :pin "8f60c9bf124ab9597d681504a73fdf116a0bde12"))
 
 ;; TODO flymake?
